2cdbfa9839113f4175595678c74428208be927d3
jr <jrewienski>
2020-09-02 02:26:48 +0200

    Change dir in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ ls
 pwd
 echo "Hello world!"
 cd pca10059/mbr/armgcc
-sed -i 's/SDK_ROOT :=.*/SDK_ROOT := \/tmp\/nRF5_SDK_17.0.0/' Makefile
+sed -i 's/SDK_ROOT :=.*/SDK_ROOT := \/tmp\/nRF5_SDK_17.0.0_9d13099/' Makefile
 make
